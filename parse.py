2ea286eb54e06caea50393ba1f01fa5e379e0b8a
yhf <xdhfying@gmail.com>
2015-07-27 09:59:45 +0800

    parse json

--- parse.py
+++ parse.py
@@ -1,16 +1,23 @@
 import json
 
 
-with open('data.json', 'r') as f:
-    companies = json.loads(f.read())['companies']
-    for com in companies:
-        com['job_types'] = [k for (k, v) in com['job_types'].items() if v]
-        com['degrees'] = [k for (k, v) in com['degrees'].items() if v]
-        com['authorizations'] = [k for (k, v) in com['authorizations'].items() if v]
-        if com['website'].startswith("http://http://") or com['website'].startswith("http://https://"):
-            com['website'] = com['website'][7:]
+def parse():
+    """parse the json.
+    1. convert `map` to `list`.
+    2. fix url error.
+    """
+    with open('data.json', 'r') as f:
+        companies = json.loads(f.read())['companies']
+        for com in companies:
+            com['job_types'] = [k for (k, v) in com['job_types'].items() if v]
+            com['degrees'] = [k for (k, v) in com['degrees'].items() if v]
+            com['authorizations'] = [k for (k, v) in com['authorizations'].items() if v]
+            if com['website'].startswith("http://http://") or com['website'].startswith("http://https://"):
+                com['website'] = com['website'][7:]
 
-with open('another.json', 'w') as f:
-    f.write(json.dumps(companies))
+    with open('another.json', 'w') as f:
+        f.write(json.dumps(companies))
 
-print "yes"
+
+if __name__ == '__main__':
+    parse()
